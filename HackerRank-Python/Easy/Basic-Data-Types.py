b96d3034065f7c920974a871e1d6e9fe0831b78f
Tanmay Unhale <83015537+Tanmay9993@users.noreply.github.com>
2025-03-19 17:02:03 -0400

    Create Basic-Data-Types.py

---
+++ HackerRank-Python/Easy/Basic-Data-Types.py
@@ -0,0 +1,14 @@
+# 1. Runner-Up Score
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    arr_list = list(set(arr))
+    arr_list.sort()
+    print(arr_list[-2])
+
+
+# 2. 
+
+
+
+
