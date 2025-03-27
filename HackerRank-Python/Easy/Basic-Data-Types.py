bd33062d1c58c806fba9af89e09988093b4b0de8
Tanmay Unhale <83015537+Tanmay9993@users.noreply.github.com>
2025-03-26 23:05:59 -0400

    Update Basic-Data-Types.py

--- HackerRank-Python/Easy/Basic-Data-Types.py
+++ HackerRank-Python/Easy/Basic-Data-Types.py
@@ -7,7 +7,31 @@ if __name__ == '__main__':
     print(arr_list[-2])
 
 
-# 2. 
+# 2. Nested List
+if __name__ == '__main__':
+    
+    # Build nested list
+    students = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name,score])
+        
+    # Get unique score and secon lowest
+    all_scores = sorted(set([score for name, score in students]))
+    second_lowest_score = all_scores[1]
+
+    # Get names who have second highest score
+    second_lowest_name = [name for name, score in students if score == second_lowest_score]
+    
+    # Sort and print
+    second_lowest_name.sort()
+    for i in second_lowest_name:
+        print(i)
+
+
+# 3. 
+       
 
 
 
