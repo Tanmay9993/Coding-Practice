9ee7e7de262fe931559b85721679a77758bc7995
Tanmay Unhale <83015537+Tanmay9993@users.noreply.github.com>
2025-07-08 19:59:11 -0400

    Update Strings.py

--- HackerRank-Python/Easy/Strings.py
+++ HackerRank-Python/Easy/Strings.py
@@ -22,4 +22,26 @@ def print_full_name(first, last):
     print(f"Hello {first} {last}! You just delved into python.")
 
 
-# 4. 
+# 4. Mutation
+def mutate_string(string, position, character):
+    new_string = string[:position] + character + string[position+1:]
+    return new_string
+
+    
+# 5. Find a string
+def count_substring(string, sub_string):
+    counter = 0
+    for i in range(0,len(string)-len(sub_string)+1):
+        if string[i:i+len(sub_string)] == sub_string:
+            counter = counter + 1
+    return counter
+
+
+# 6. String Validators
+if __name__ == '__main__':
+    s = input()
+    print(any(c.isalnum() for c in s))    # Alphanumeric
+    print(any(c.isalpha() for c in s))    # Alphabetical
+    print(any(c.isdigit() for c in s))    # Digits
+    print(any(c.islower() for c in s))    # Lowercase
+    print(any(c.isupper() for c in s))    # Uppercase
