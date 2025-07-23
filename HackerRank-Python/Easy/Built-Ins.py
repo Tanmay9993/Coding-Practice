046c73d8ee2fc65385aea0df067aa9d157d82011
Tanmay Unhale <83015537+Tanmay9993@users.noreply.github.com>
2025-07-23 16:09:22 -0400

    Update Built-Ins.py

--- HackerRank-Python/Easy/Built-Ins.py
+++ HackerRank-Python/Easy/Built-Ins.py
@@ -1,19 +1,25 @@
 
 ### 1. Zipped
 
+
 num_scores, records = map(int, input().split())
 all_score = []
 for i in range(0,records):
     values = list(map(float, input().split()))
-    all_score.append(values[0:5])    
-    
-# Calculate the average
+    all_score.append(values)    
+
+# Calculate Avg Method - 1
+transpose= list(zip(*all_score))
+for i in transpose:
+    value = sum(i)/records
+    print(value)
+
+
+# Calculate Avg Method - 2
 for i in range(0,num_scores):
-    
     sums = 0
     for j in range(0, records):
         sums = sums + all_score[j][i]
-    
     avg = round((sums/records),1)
     print(avg)
 
